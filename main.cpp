93fd556b5f1c7a50a260f9eebf1d887f393ec8df
darryl-ty <darrylterrell921@gmail.com>
2023-06-18 01:38:36 -0400

    Reorder main.cpp to be more clear and parseable.

--- main.cpp
+++ main.cpp
@@ -1,30 +1,50 @@
 #include <iostream>
 #include <string>
+#include <filesystem>
 #include "StudentInfo.h"
 
-
-void PrintTerminalArt(){
-    std::cout << "Welcome to the Degree Audit System!" << std::endl;
-    std::cout
-    << R"( _____   ______  ______  ______  ______  ______
-/\  __-./\  ___\/\  ___\/\  == \/\  ___\/\  ___\
-\ \ \/\ \ \  __\\ \ \__/\ \  __<\ \  __\\ \  __\
-\ \____-\ \_____\ \_____\ \_\ \_\ \_____\ \_____\
-\/____/ \/_____/\/_____/\/_/ /_/\/_____/\/_____/)"
-    << std::endl;
-
-    std::cout
-    << R"(  ______  __  __  _____   __  ______
- /\  __ \/\ \/\ \/\  __-./\ \/\__  _\
- \ \  __ \ \ \_\ \ \ \/\ \ \ \/_/\ \/
-  \ \_\ \_\ \_____\ \____-\ \_\ \ \_\
-  \/_/\/_/\/_____/\/____/ \/_/  \/_/ )"
-    << std::endl;
-}
+void PrintTerminalArt();
+void CreateTranscriptPath();
+void DegreeAudit();
 
 StudentInfo TryParseInt(std::string *inputStr);
 StudentInfo UserInput();
 
+
+int main() {
+    DegreeAudit();
+}
+
+void CreateTranscriptPath() {
+
+}
+
+void DegreeAudit() {
+    PrintTerminalArt();
+    StudentInfo student = UserInput();
+    std::cout << "Here is your student number: " << student.GetUserIdentification();
+
+    CreateTranscriptPath();
+}
+
+void PrintTerminalArt() {
+    std::cout << "Welcome to the Degree Audit System!" << std::endl;
+    std::cout
+            << R"( _____   ______  ______  ______  ______  ______
+/\  __-./\  ___\/\  ___\/\  == \/\  ___\/\  ___\
+\ \ \/\ \ \  __\\ \ \__/\ \  __<\ \  __\\ \  __\
+\ \____-\ \_____\ \_____\ \_\ \_\ \_____\ \_____\
+\/____/ \/_____/\/_____/\/_/ /_/\/_____/\/_____/)"
+            << std::endl;
+
+    std::cout
+            << R"(  ______  __  __  _____   __  ______
+ /\  __ \/\ \/\ \/\  __-./\ \/\__  _\
+ \ \  __ \ \ \_\ \ \ \/\ \ \ \/_/\ \/
+  \ \_\ \_\ \_____\ \____-\ \_\ \ \_\
+  \/_/\/_/\/_____/\/____/ \/_/  \/_/ )"
+            << std::endl;
+}
 
 StudentInfo TryParseInt(std::string *inputStr) {
     int convertedNum;
@@ -55,15 +75,4 @@ StudentInfo UserInput() {
     std::cin >> inputStudentNumber;
 
     return TryParseInt(&inputStudentNumber);
-}
-
-void DegreeAudit(){
-    PrintTerminalArt();
-    StudentInfo student = UserInput();
-    std::cout << "Here is your student number: " << student.GetUserIdentification();
-}
-
-
-int main() {
-    DegreeAudit();
 }
